1c125a0ab0d43e3282e6f6431bc0c40ef8901ed7
Mike Molina <leo318x@gmail.com>
2022-09-21 19:42:04 -0600

    C3D Logica

--- Expresion/Logica.py
+++ Expresion/Logica.py
@@ -27,14 +27,29 @@ class Logica(Expresion):
             return resultado
 
     def traducir(self, entorno, C3D):
-        valorIzq = self.exprIzq.traducir(entorno)
-        valorDer = self.exprDer.traducir(entorno)
+        valorIzq = self.exprIzq.traducir(entorno, C3D)
+        valorDer = self.exprDer.traducir(entorno, C3D)
 
         if TIPO_LOGICO.AND == self.tipo_operacion:
             resultado = Retorno(valorIzq.valor and valorDer.valor, TIPO_DATO.BOOL)
             return resultado
         elif TIPO_LOGICO.OR == self.tipo_operacion:
-            t_actual = C3D.getT()
-            C3D.agregarTraduccion(f'if (t{t_actual}) goto')
+            print(f'c3d_or_izq: {valorIzq.valor}')
+            print(f'c3d_or_der: {valorDer.valor}')
+            t_actual = t_actual = C3D.getT()
+            C3D.sumarT()
+            l1 = C3D.getNuevoL()
+            l2 = C3D.getNuevoL()
+            l3 = C3D.getNuevoL()
+            l4 = C3D.getNuevoL()
+
+            C3D.agregarTraduccion(f'if ({valorIzq.valor}) goto L{l1};')
+            C3D.agregarTraduccion(f'goto L{l2};')
+
+            C3D.agregarTraduccion(f'L{l2}:')
+            C3D.agregarTraduccion(f'if ({valorDer.valor}) goto L{l3};')
+            C3D.agregarTraduccion(f'goto L{l4};')
+            C3D.agregarTraduccion(f'L{l1}:')
+            C3D.agregarTraduccion(f'L{l3}:')
             resultado = Retorno(valorIzq.valor or valorDer.valor, TIPO_DATO.BOOL)
             return resultado
